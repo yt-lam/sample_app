2896731bc6f9cb047fad95a0d21871fe05a1d8d1
Roark Lam <teckrieg@gmail.com>
2011-05-18 19:10:06 +0800

    Finished layout and routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "should  have a Home page at '/" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+  
+  it "should  have a Contact page at '/" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should  have a About page at '/" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end    
+
+  it "should  have a Help page at '/" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end  
+  
+  it "should have a signup page at '/signup'" do
+     get '/signup'
+     response.should have_selector('title', :content => "Sign up")
+  end
+  
+end
